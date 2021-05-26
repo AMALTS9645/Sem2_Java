7893a2f593ae663206188994e6cc1f1ee7c54a6d
AMALTS9645 <amaltsthekkel@gmail.com>
2021-05-27 00:04:44 +0530

    added classes and object simple program

---
+++ Class and object/Box/Box.java
@@ -0,0 +1,17 @@
+class Box
+{
+int width; 
+int height; 
+int depth;
+
+public static void main(String a[])
+{
+Box mybox=new Box(); 
+int vol; 
+mybox.width=10; 
+mybox.height=20; 
+mybox.depth=30;
+vol=mybox.width*mybox.height*mybox.depth;
+System.out.println("Volume is "+vol);
+}
+}
---
+++ Class and object/BoxDemo/BoxDemo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class Box
+{
+int width; 
+int height; 
+int depth;
+}
+class BoxDemo
+{
+public static void main(String a[])
+{
+Box mybox=new Box(); 
+int vol; 
+mybox.width=10; 
+mybox.height=20; 
+mybox.depth=30;
+vol=mybox.width*mybox.height*mybox.depth;
+System.out.println("Volume is :"+vol);
+}
+}
+
