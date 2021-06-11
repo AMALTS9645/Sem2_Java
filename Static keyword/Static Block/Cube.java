0c3f19cffd633682212673ab2d3a1d0923686418
AMAL T S <amaltsthekkel@gmail.com>
2021-06-11 15:31:18 +0530

    static keyword

---
+++ Static keyword/Static Block/Cube.java
@@ -0,0 +1,12 @@
+class Cube
+{
+static //first execute static block
+{
+int a = 3*3*3;
+System.out.println("Cube is:" +a);
+}
+public static void main(String [] args)
+{
+System.out.println("Hello Main");
+}
+}
