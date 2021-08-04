09881b705194faeaa7979dd433cdede61dd436df
AMAL T S <amaltsthekkel@gmail.com>
2021-08-04 19:09:05 +0530

    cycle 2 edited

--- lab cycle 2/01.Sort/Sort.java
+++ lab cycle 2/01.Sort/Sort.java
@@ -1,44 +1,37 @@
-import java.util.Scanner;
-public class Sort
-{
-    public static void main(String[] args) 
-    {
-        int count;
-        String temp;
-        Scanner scan = new Scanner(System.in);
-        
-     
-        System.out.print("Enter number of strings you would like to enter:");
-        count = scan.nextInt();
-        
-        
-        String str[] = new String[count];
-        Scanner scan2 = new Scanner(System.in);
-        
-        System.out.println("Enter the Strings one by one:");
-        for(int i = 0; i < count; i++)
-        {
-            str[i] = scan2.nextLine();
-        }
-        scan.close();
-        scan2.close();
-  
-        for (int i = 0; i < count; i++) 
-        {
-            for (int j = i + 1; j < count; j++) { 
-                if (str[i].compareTo(str[j])>0) 
-                {
-                    temp = str[i];
-                    str[i] = str[j];
-                    str[j] = temp;
-                }
-            }
-        }
+import java.util.*;
 
-        System.out.print("Strings in Sorted Order:");
-        for (int i = 0; i <= count - 1; i++) 
-        {
-            System.out.print(str[i] + ", ");
-        }
-    }
+class Sort
+{
+public static void main(String args[]) 
+{
+int n;
+String temp;
+Scanner s = new Scanner(System.in);
+System.out.print("enter the number of strings : ");
+n=s.nextInt();
+String str[] = new String[n];
+System.out.println("enter the strings : ");
+for(int i=0; i<n; i++)
+{
+str[i] = s.next();
+}
+for(int i=0 ; i<n ; i++) 
+{
+for (int j=i+1 ; j<n ; j++) 
+{
+if (str[i].compareTo(str[j])>0) 
+{
+temp=str[i];
+str[i]=str[j];
+str[j]=temp;
+}
+}
+}
+System.out.println("strings in sorted order : ");
+for (int i=0 ; i<n-1; i++) 
+{ 
+System.out.println(str[i]);
+}
+System.out.print(str[n-1]);
+}
 }
--- lab cycle 2/05.Student_Details/Student_Details.java
+++ lab cycle 2/05.Student_Details/Student_Details.java
@@ -1,59 +1,108 @@
-import java.util.Scanner;
-public class Student_Details
+import java.util.*;
+
+class Student_Details
 {
-public static void main(String args[])
-{
-int i,j,n;
-float total=0, avg;
-Scanner s1 = new Scanner(System.in);
-Scanner s2 = new Scanner(System.in);
-Scanner s3 = new Scanner(System.in);
-Scanner s4 = new Scanner(System.in);
+int tot,i,avg,yr,sid;
+String sname,branch;
+int marks[]= new int[6];
 Scanner s = new Scanner(System.in);
-System.out.print("Enter no. of student details:");n = s.nextInt();
-int stuid[] = new int[n];
-String name[] = new String[n];
-String branch[] = new String[n];
-int year[] = new int[n];
-for(i = 0; i< n; i++)
+public void getdata() 
 {
-System.out.println("Enter the id of the student:");
-stuid[i]=s1.nextInt();
-System.out.println("Enter the name of the student:");
-name[i] = s2.nextLine();
-System.out.println("Enter the branch of the student:");
-branch[i]=s3.nextLine();
-System.out.println("Enter the year of study:");
-year[i]=s4.nextInt();
-int marks[] = new int[6];
-for(j=0; j<6; j++)
-{
-System.out.println("Enter Marks of Subject"+(j+1)+":");
-marks[j] = s1.nextInt();
-total = total + marks[j];
+System.out.println("\nenter the student id : ");
+sid=s.nextInt();
+System.out.println("enter the student name : ");
+sname=s.next();
+System.out.println("enter the student branch : ");
+branch=s.next();
+System.out.println("enter the student year : ");
+yr=s.nextInt();
+
+for(i=0; i<6; i++) 
+{ 
+System.out.print("enter marks of subject"+(i+1)+" : ");
+marks[i]=s.nextInt();
+tot=tot+marks[i];
 }
-avg = total/6;
-System.out.println("The student Grade is: ");
+avg=tot/6;
+}   
+
+public void display() 
+{
+System.out.println("\nstudent id : " +sid);
+System.out.println("student name : " +sname);
+System.out.println("student branch : " +branch);
+System.out.println("student yr : " +yr);
 if(avg>=80)
 {
-System.out.println("A");}
-else if(avg>=60 &&avg<80)
-{
-System.out.println("B");
+System.out.print("\nstudent grade is A.\n");
 }
-else if(avg>=40 &&avg<60)
+else if(avg>=60 && avg<80)
 {
-System.out.println("C");
+System.out.print("\nstudent grade is B.\n");
+} 
+else if(avg>=40 && avg<60)
+{
+System.out.print("\nstudent grade is C.\n");
 }
 else
 {
-System.out.println("D");
+System.out.print("\nstudent grade is D.\n");
 }
 }
-s.close();
-s1.close();
-s2.close();
-s3.close();
-s4.close();
+public static void main(String args[])
+{
+int n=0,maxi1=0,maxi2=0;
+float max1=0,max2=0;
+Scanner s1 = new Scanner(System.in);
+System.out.print("enter the limit : ");
+n=s1.nextInt();
+Student s[]=new Student[n];
+for(int i=0; i<n; i++) 
+{
+s[i]=new Student();
+s[i].getdata();
+}
+System.out.println("\n***STUDENT DETAILS***");
+for(int i=0; i<n; i++) 
+{
+s[i].display();
+}
+for(int i=1;i<n;i++)
+{
+if(s[i].avg<s[i-1].avg)
+{
+max1=s[i].avg;
+maxi1=i;
+}
+else
+{
+max1=s[i-1].avg;
+maxi1=i-1;
+}
+max2=max1;
+maxi2=maxi1;
+}
+for(int i=0; i<n; i++) 
+{
+if(max1<=s[i].avg)
+{
+max1=s[i].avg;
+maxi1=i;
+}
+}
+for(int i=0; i<n; i++) 
+{
+if(max2<=s[i].avg && max1!=s[i].avg)
+{
+max2=s[i].avg;
+maxi2=i;
+}
+}
+System.out.println("\n***FIRST POSITION***");
+s[maxi1].display();
+System.out.println("average marks : "+max1);
+System.out.println("\n***SECOND POSITION***");
+s[maxi2].display();
+System.out.println("average marks : "+max2);
 }
 }
